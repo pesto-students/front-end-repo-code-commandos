a285b73c583877ed65947d1af0a311807e4e957e
HarshilVadgama31 <vadgamaharshil99@gmail.com>
2024-01-06 10:57:02 +0530

    Changed host to heroku from netlify

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,2 +1,2 @@
-// export const host = "https://match-made-9322a21d710e.herokuapp.com";
-export const host = "https://6598ab5302b53d75fd98013b--majestic-rolypoly-eef9a5.netlify.app";
+export const host = "https://match-made-9322a21d710e.herokuapp.com";
+// export const host = "https://6598ab5302b53d75fd98013b--majestic-rolypoly-eef9a5.netlify.app";
