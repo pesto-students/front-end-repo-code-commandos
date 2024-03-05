f35a3feba79c23cb48a5ba1748dec08853b8ba07
HarshilVadgama31 <vadgamaharshil99@gmail.com>
2024-03-05 15:18:30 +0530

    Router Testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App.jsx";
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello Home</div>,
-  },
-  {
-    path: "/login",
-    element: <div>Hello Login</div>,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
